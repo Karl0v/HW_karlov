e611228da539b3193cf3e547c3be3fc22aa1ffc4
Karl0v <112897240+Karl0v@users.noreply.github.com>
2022-10-01 16:06:25 +0300

    # офіційне письмо 1.1

--- Home work/Lesson 5/L5_HW2.py
+++ Home work/Lesson 5/L5_HW2.py
@@ -1,28 +1,27 @@
 # офіційне письмо
-while True:
 
-    name_recipient = input("Введіть, будь ласка, Ім'я одержувача >> ")
-    lastname_recipient = input("Введіть, будь ласка, Прізвище одержувача >> ")
-    name_sender = input("Введіть, будь ласка, Ім'я відправника >> ")
-    lastname_sender = input("Введіть, будь ласка, Прізвище відправника >> ")
-    position_sender = input("Введіть, будь ласка, Посаду відправника >> ")
+email = '''Dear *Ім'я одержувача* *Прізвище одержувача*,
+   
+  We are lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum in faucibus massa. 
+  Suspendisse at ex varius, porttitor eros sit amet, sagittis nibh. In vel est a tortor tempor luctus a. 
+   
+  ________________
+  *Ім'я відправика* *Прізвище відправника*
+  *Посада відправника*'''
 
-    email = f'''
-Dear {name_recipient.capitalize()} {lastname_recipient.capitalize()},
+name_recipient = input("Введіть, будь ласка, Ім'я одержувача >> ")
+lastname_recipient = input("Введіть, будь ласка, Прізвище одержувача >> ")
+name_sender = input("Введіть, будь ласка, Ім'я відправника >> ")
+lastname_sender = input("Введіть, будь ласка, Прізвище відправника >> ")
+position_sender = input("Введіть, будь ласка, Посаду відправника >> ")
 
-We are lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum in faucibus massa.
-Suspendisse at ex varius, porttitor eros sit amet, sagittis nibh. In vel est a tortor tempor luctus a.
+def paste(email):
+    a = email.replace("*Ім'я одержувача*", name_recipient.capitalize())
+    b = a.replace("*Прізвище одержувача*", lastname_recipient.capitalize())
+    c = b.replace("*Ім'я відправика*", name_sender.capitalize())
+    d = c.replace("*Прізвище відправника*", lastname_sender.capitalize())
+    e = d.replace("*Посада відправника*", position_sender.capitalize())
+    return print(e)
 
-________________
-{name_sender.capitalize()} {lastname_sender.capitalize()},
-{position_sender.capitalize()}'''
 
-    print(email)
-
-    x = input('''
-            Щоб розпочати знову натисніть любу клавішу
-               Якщо бажаєте закінчити натисніть - х
-                >> ''')
-    if x.lower() == 'x':
-        break
-    continue
+paste(email)
