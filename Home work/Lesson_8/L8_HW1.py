3d7a29da65b59569fbdd15e10f09ccfd073198ae
Karl0v <112897240+Karl0v@users.noreply.github.com>
2022-10-10 19:12:02 +0300

    #Нотатник у файлі 1.1

--- Home work/Lesson_8/L8_HW1.py
+++ Home work/Lesson_8/L8_HW1.py
@@ -1,17 +1,30 @@
 import os
 
+
 def info():
     """
  Функція нагадує користувачу про можливості програми
     :return: виводить на екран можливості
     """
-    info = '''Що вміє ця програма:
+    print('''
+Що вміє ця програма:
 save - зберігає всі активні нотатки у службовий файл
 load - завантаження збережених нотаток зі службового файла
 clear - видалити всі збережені нотатки та видалити файл зберігання нотаток
 info - можливості програми
-exit - вихід з програми'''
-    return print(info)
+exit - вихід з програми''')
+
+
+def infoin():
+    print('''
+Як ви бажаєте відобразити нотатки? 
+Для запису нотатки натисніть - "Enter"
+Для відображення від найранішої до найпізнішої - "earliest"
+Для відображення від найпізнішої до найранішої - "latest"
+Для відображення від найдовшої до найкоротшої - "longest"
+Для відображення від найкоротшої до найдовшої - "shortest"
+Якщо щось забули - "info"
+Вийти - "x"''')
 
 
 info()
@@ -37,6 +50,45 @@ def loading():
         print(fp.read())
 
 
+def latest():
+    """
+    Відображення наповнення файла від найпізнішої до найранішої нотатки
+
+    :return: відображає результат
+    """
+    with open(notes, mode='r') as fp:
+        for text in reversed(list(fp)):
+            print(text.strip('\n'))
+
+
+'''def bylength(notes):
+    with open(notes, mode='r') as fp:
+        for text in reversed(list(fp)):
+            print(len(text))'''
+
+
+def longest():
+    """
+      Відображення наповнення файла від найдовшої до найкоротшої
+
+      :return: відображає результат
+      """
+    with open(notes, mode='r') as fp:
+        for text in sorted(list(fp), key=len, reverse=True):
+            print(text.strip('\n'))
+
+
+def shortest():
+    """
+        Відображення наповнення файла від найкоротшої до найдовшої
+
+        :return: відображає результат
+        """
+    with open(notes, mode='r') as fp:
+        for text in sorted(list(fp), key=len, reverse=False):
+            print(text.strip('\n'))
+
+
 def clear():
     """
     Видаляє всі збережені нотатки та сам файл зберігання нотаток
@@ -45,16 +97,38 @@ def clear():
     os.remove(notes)
 
 
-
 while True:
-    user_choise = input('Яку дію бажаєте виконати: > save > load > clear > info > exit >> ')
+    user_choise = input('\nЯку дію бажаєте виконати: > save > load > clear > info > exit >> ')
     if user_choise.lower() == 'save':
         save()
     elif user_choise.lower() == 'load':
-        try:
-            loading()
-        except FileNotFoundError:
-            print('\nЗавантажити нотатки не виходить, так як файл з ними був видалений\n')
+        infoin()
+        while True:
+            start = input('''
+Для нотатки натисніть - "Enter"
+або інший варіант -> ''')
+            try:
+                if start == '':
+                    save()
+                elif start.lower() == 'earliest':
+                    loading()
+                elif start.lower() == 'latest':
+                    latest()
+                elif start.lower() == 'longest':
+                    longest()
+                elif start.lower() == 'shortest':
+                    shortest()
+                elif start.lower() == 'info':
+                    info()
+                elif start.lower() == 'x':
+                    break
+                else:
+                    print('Якщо щось забули - "i"')
+                '''
+                loading()'''
+            except FileNotFoundError:
+                print('\nЗавантажити нотатки не виходить, так як файл з ними був видалений\n')
+
     elif user_choise.lower() == 'clear':
         try:
             ask = input('Ви впевнені, що хочете видалити файл y/n?\n >>> ')
